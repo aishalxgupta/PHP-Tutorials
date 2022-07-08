5c6f04c4d733f6c4b72f48a9a1947cbe525ff04e
Aishal Gupta <aishalgupta3008@gmail.com>
2022-07-08 10:09:37 +0530

    basic login frontend

---
+++ Student_Info.php
@@ -0,0 +1,20 @@
+
+
+<html>
+<body>
+
+<h2>Login Page</h2>
+<form action="POST.php" method="post">
+<b> Username : </b> <input type="text" name="username">
+
+<b> Password : </b> <input type="password" name="password">
+
+<br>
+<input type="submit">
+</form>
+
+
+
+</body>
+</html>
+
