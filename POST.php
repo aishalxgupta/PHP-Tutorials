8199b08aeecf3ee965d99c5dbba01652ac0aff4e
Aishal Gupta <aishalgupta3008@gmail.com>
2022-07-08 10:10:00 +0530

    basic login backend

---
+++ POST.php
@@ -0,0 +1,14 @@
+<?php
+
+
+$_POST["username"];
+
+$_POST["password"];
+
+if (($_POST["username"]==="aishal") && ($_POST["password"]==="pass") ){
+	echo "Bingo!";
+}
+else{
+	echo "Try Again!";
+}
+?>
