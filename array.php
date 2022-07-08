a2a8b39fa7b351a2c31760acdf8daa1f5d93489b
Aishal Gupta <aishalgupta3008@gmail.com>
2022-07-08 09:29:06 +0530

    array

---
+++ array.php
@@ -0,0 +1,16 @@
+<?php
+
+$names = array("Ria","Jia","Sia");
+
+echo $names[1];
+echo "<br>";
+print_r($names);
+echo "<br>";
+# dictoionary
+
+$marks=array("a" => 23, "b" => 34);
+print_r($marks);
+
+
+
+?>
